ad6324ce8c42b52446e20e443cc7ead8a67bbc55
christopher_carman <chris.r.carman@gmail.com>
2015-03-13 13:34:50 -0600

    HealthLevelTransfer
    The device to which will have the health transfer between levels. This
    is also a singleton

---
+++ HauntedHouse4.5/Assets/Scripts/HealthLevelTransfer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthLevelTransfer : MonoBehaviour {
+
+	private static HealthLevelTransfer _instance;
+	int health;
+	//Construct  
+	private HealthLevelTransfer(){
+		health=100;
+	}
+
+	//  Instance 
+	public static HealthLevelTransfer Instance
+	{
+		get{
+			if(_instance == null)
+			{
+				_instance=GameObject.FindObjectOfType(typeof(HealthLevelTransfer)) as HealthLevelTransfer; 
+				
+				DontDestroyOnLoad(_instance.gameObject);
+			}
+			return _instance;
+		}
+	}
+	void Awake()
+	{
+		if(_instance == null)
+		{
+			//If I am the first instance, make me the Singleton
+			_instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			//If a Singleton already exists and you find
+			//another reference in scene, destroy it!
+			if(this != _instance)
+				Destroy(this.gameObject);
+		}
+	}
+	
+	public int getHealth()
+	{
+		return health;
+	}
+	public void setHealth(int newHealth)
+	{
+		health=newHealth;
+	}
+	public void resetHealth()
+	{
+		health=100;
+	}
+}

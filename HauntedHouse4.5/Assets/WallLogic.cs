1b9d18f99aaccd2e056dfb8215450c3a5e3a4b25
christopher_carman <chris.r.carman@gmail.com>
2015-03-13 17:08:01 -0600

    yachans changes
    Add patrol to enemytype one

---
+++ HauntedHouse4.5/Assets/WallLogic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallLogic : MonoBehaviour {
+
+	public move move;
+	public Transform target;
+	public int moveSpeed = 1;
+
+	void Start ()
+	{
+		GameObject go = GameObject.FindGameObjectWithTag("Enemy");
+		target = go.transform;
+
+		
+		move = (move)go.GetComponent (typeof(move));
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Enemy") 
+		{
+			//move.RotationWalk();
+			Quaternion newRotation = Quaternion.AngleAxis(180, Vector3.up);
+			target.rotation= Quaternion.Slerp(target.rotation, newRotation, .05f);  
+			//walk forward
+			target.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+		}
+	}
+
+}
